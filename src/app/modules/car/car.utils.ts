acf1a453e72467a25eccba22238212d5bd4e93c8
Moshfiqur Rahman <studentemam@gmail.com>
2026-01-02 17:48:00 +0600

    feat: add distance field

--- src/app/modules/car/car.utils.ts
+++ src/app/modules/car/car.utils.ts
@@ -4,6 +4,7 @@ import { CAR_STATUS } from "../booking/booking.interface";
 import { CarServices } from "./car.service";
 import { CAR_VERIFICATION_STATUS } from "./car.interface";
 import ApiError from "../../../errors/ApiErrors";
+import { User } from "../user/user.model";
 
 export const getCarTripCount = async (
     carId: Types.ObjectId | string
@@ -123,4 +124,28 @@ export const normalizeCarVerificationStatus = (
   }
 
   return normalized as CAR_VERIFICATION_STATUS;
+};
+
+
+export const getTargetLocation = async (queryLat?: string | number, queryLng?: string | number, userId?: string) => {
+
+  let lat = queryLat ? Number(queryLat) : null;
+  let lng = queryLng ? Number(queryLng) : null;
+
+
+  if ((!lat || !lng) && userId) {
+    const user = await User.findById(userId).select("location");
+    if (user?.location?.coordinates) {
+      lng = user.location.coordinates[0];
+      lat = user.location.coordinates[1];
+    }
+  }
+
+// default dhaka
+  if (!lat || !lng) {
+    lng = 90.4125; 
+    lat = 21.8103;
+  }
+
+  return { lat, lng };
 };
